2d1c22d121a6d8f72096cb5251c114dbb18ea8c3
NadimBhuiyan34 <nadimbhuiyan15900@gmail.com>
2024-12-21 21:01:04 +0600

    model

--- NBSoft.Application/Interfaces/IRepository/IUnitOfWork.cs
+++ NBSoft.Application/Interfaces/IRepository/IUnitOfWork.cs
@@ -9,7 +9,9 @@ namespace NBSoft.Application.Interfaces.IRepository
 {
     public interface IUnitOfWork : IDisposable
     {
-        IBaseRepository<T> Repository<T>() where T : BaseAuditableEntity;
+        IBaseRepository<T> Repository_BaseAuditableEntity<T>() where T : BaseAuditableEntity;
+        IBaseRepository<T> Repository<T>() where T : BaseModel;
+
         Task<int> Save(CancellationToken cancellationToken);
 
         Task<int> SaveAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys);
